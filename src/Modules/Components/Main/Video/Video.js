b86faf99c0e7bfcbd54dfcc8035d70f8a29d6060
Jordion69 <jortiznavas0@gmail.com>
2022-03-18 15:18:21 +0100

    update prototype

--- src/Modules/Components/Main/Video/Video.js
+++ src/Modules/Components/Main/Video/Video.js
@@ -3,10 +3,11 @@ import React from 'react'
 export default function Video() {
   return (
 	<div className='container'>
-		<div>
-			<video loop autoPlay muted>
+		<div className=''>
+			{/* <video loop autoPlay muted>
 				<source src="https://www.youtube.com/watch?v=ChuVhTcXA2c" type="video/mp4" />
-			</video>
+			</video> */}
+			<iframe className='embed-responsive-item'width="560" height="315" src="https://www.youtube.com/embed/qLyi4-3pUQM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		</div>
 	</div>
   )
